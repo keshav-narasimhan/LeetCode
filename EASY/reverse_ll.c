13f9ec974bd77861a014ed2bf5cd0c2811699516
Keshav Narasimhan <52009148+keshav-narasimhan@users.noreply.github.com>
2021-03-22 12:55:11 -0500

    Add files via upload

---
+++ EASY/reverse_ll.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Definition for singly-linked list.
+    struct ListNode {
+    int val;
+    struct ListNode *next;
+ };
+
+struct ListNode* reverseList(struct ListNode* head){
+    
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    struct ListNode *next = NULL;
+    
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    
+    return head;
+    
+}
