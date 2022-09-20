b4a84d207c54bb388215379938276493cb1f5b16
Keshav Narasimhan <52009148+keshav-narasimhan@users.noreply.github.com>
2022-09-19 22:55:31 -0500

    Update TwoSum.java

--- EASY/TwoSum.java
+++ EASY/TwoSum.java
@@ -1,51 +1,36 @@
-/* 
- * EASY 2/20/2021
- */
-
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int [] targ_arr = new int[2];
-        
-//         outerloop:
-//         for (int a = 0; a < nums.length; a++)
-//         {
-//             targ_arr[0] = a;
-//             for (int b = a + 1; b < nums.length; b++)
-//             {
-//                 if (nums[a] + nums[b] == target)
-//                 {
-//                     targ_arr[1] = b;
-//                     break outerloop;
-//                 }
-//             }
-//         }   
-//         return targ_arr;
-//     }
-// }
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> n = new HashMap<>();
-        int [] index = new int[2];
-        
+        /*
+        // Brute Force
         for (int i = 0; i < nums.length; i++) {
-            if (n.containsKey(nums[i])) {
-                if (n.get(nums[i]) == -1) {
-                    index[0] = i;
-                    index[1] = n.get(target - nums[i]);
-                } else if (n.get(nums[i]) == n.get(target - nums[i])) {
-                    index[0] = i;
-                    index[1] = n.get(nums[i]);
+            int first = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                int second = nums[j];
+                if (first + second == target) {
+                    int [] result = { i, j };
+                    return result;
                 }
-            } else {
-                n.put(nums[i], i);
-                if (target - nums[i] != nums[i]) {
-                    n.put(target-nums[i], -1);
-                }
-                
             }
         }
         
-        return index;
+        int [] result = new int[2];
+        return result;
+        */
+        
+        // Optimal
+        HashMap<Integer, Integer> values = new HashMap<>();
+        int [] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (values.containsKey(nums[i])) {
+                int j = values.get(nums[i]);
+                result[0] = j;
+                result[1] = i;
+                return result;
+            } else {
+                values.put(target - nums[i], i); 
+            }
+        }
+        
+        return new int[2];
     }
 }
